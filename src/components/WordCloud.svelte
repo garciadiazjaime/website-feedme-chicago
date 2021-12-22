6b55d3f9a1cb26038cd534ba82b1beefc1035d2a
jaime <garciadiazjaime@gmail.com>
2021-12-21 22:39:17 -0600

    feat: world cloud

---
+++ src/components/WordCloud.svelte
@@ -0,0 +1,59 @@
+<script>
+  import { onMount } from 'svelte'
+
+  export let data
+
+  let el
+  onMount(async({
+  size = group => group.length, // Given a grouping of words, returns the size factor for that word
+  word = d => d, // Given an item of the data array, returns the word
+  marginTop = 0, // top margin, in pixels
+  marginRight = 0, // right margin, in pixels
+  marginBottom = 0, // bottom margin, in pixels
+  marginLeft = 0, // left margin, in pixels
+  width = 1200, // outer width, in pixels
+  height = 600, // outer height, in pixels
+  maxWords = 250, // maximum number of words to extract from the text
+  fontFamily = "sans-serif", // font family
+  fontScale = 11, // base font size
+  padding = 0, // amount of padding between the words (in pixels)
+  rotate = 0, // a constant or function to rotate the words
+  invalidation // when this promise resolves, stop the simulation
+  } = {}) => {
+    // const words = typeof text === "string" ? text.split(/\W+/g) : Array.from(text);
+  
+    // const data = d3.rollups(words, size, w => w)
+    //   .sort(([, a], [, b]) => d3.descending(a, b))
+    //   .slice(0, maxWords)
+    //   .map(([key, size]) => ({text: word(key), size}));
+    console.log('data', data)
+    
+    const svg = d3.select(el)
+      .append("svg")
+      .attr("viewBox", [0, 0, width, height])
+      .attr("width", width)
+      .attr("font-family", fontFamily)
+      .attr("text-anchor", "middle")
+      .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+
+    const g = svg.append("g").attr("transform", `translate(${marginLeft},${marginTop})`);
+
+    const cloud = d3Cloud()
+        .size([width - marginLeft - marginRight, height - marginTop - marginBottom])
+        .words(data)
+        .padding(padding)
+        .rotate(rotate)
+        .font(fontFamily)
+        .fontSize(d => Math.sqrt(d.size) * fontScale)
+        .on("word", ({size, x, y, rotate, text}) => {
+          g.append("text")
+              .attr("font-size", size)
+              .attr("transform", `translate(${x},${y}) rotate(${rotate})`)
+              .text(text);
+        });
+
+    cloud.start();
+  })
+</script>
+
+<div bind:this={el} class="chart"></div>
--- src/routes/classification.svelte
+++ src/routes/classification.svelte
@@ -4,16 +4,25 @@
 	export let classification;
 	let Bars
   let Histogram
+	let WordCloud
 
 	onMount(async() => {
 		window.d3 = await import('d3')
+		window.d3Cloud = (await import('d3-cloud')).default
 		
 		const BarsComponent = await import('../components/Bars.svelte')
 		Bars = BarsComponent.default
 
     const HistogramComponent = await import('../components/Histogram.svelte')
 		Histogram = HistogramComponent.default
+
+		const WordCloudComponent = await import('../components/WordCloud.svelte')
+		WordCloud = WordCloudComponent.default
 	})
+
+	function getWordCloudData() {
+		return classification.map(item => ({text: item.className.split(',')[0].replace(' ', '-').toLowerCase(), size: item.total}))
+	}
 </script>
 
 <script context="module">
@@ -30,6 +39,9 @@
 <svelte:head>
 	<title>Chicago Food Posts | Classifications</title>
 </svelte:head>
+
+<h2>World Cloud</h2>
+<svelte:component this={WordCloud} data={getWordCloudData()} />
 
 <h2>Histogram</h2>
 <svelte:component this={Histogram} data={classification} x={d => d.total} />
